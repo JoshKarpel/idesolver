e30f96b9ca304acae2eb681feaf1bd09128611fd
JoshKarpel <josh.karpel@gmail.com>
2020-11-19 08:59:54 -0600

    cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
